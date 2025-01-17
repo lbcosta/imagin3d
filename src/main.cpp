d83626c88ef4a336b23abcca5eccfb28f5ef115e
Leonardo Costa <lbcosta.dev@gmail.com>
2025-01-17 15:29:36 -0300

    reorganização do main

--- src/main.cpp
+++ src/main.cpp
@@ -9,38 +9,37 @@ constexpr int CANVAS_HEIGHT = 500; // pixels
 constexpr int FPS = 15;
 constexpr int N_COL = 500;
 constexpr int N_ROW = 500;
+constexpr double FIXED_Z = -30;
+vec3 FIXED_ORIGIN = 0.0;
 
 // Window/Frame
-constexpr float WINDOW_WIDTH = 60.0f;
-constexpr float WINDOW_HEIGHT = 60.0f;
+constexpr double WINDOW_WIDTH = 60.0f;
+constexpr double WINDOW_HEIGHT = 60.0f;
 
 int main() {
     InitWindow(CANVAS_WIDTH, CANVAS_HEIGHT, "Imagin3D");
     SetTargetFPS(FPS);
 
+    double deltaY = WINDOW_HEIGHT / N_ROW;
+    double deltaX = WINDOW_WIDTH / N_COL;
+
     while (!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(GRAY);
 
-        vec3 rayOrigin(0.0);
-        float sphereRadius = 40.0;
-        vec3 sphereCenter(0.0, 0.0, -100.0);
-
-        float deltaY = WINDOW_HEIGHT / N_ROW;
-        float deltaX = WINDOW_WIDTH / N_COL;
         Color pixel;
-
         for (int row = 0; row <= N_ROW; row++) {
-            float y = (WINDOW_HEIGHT/2) - (deltaY/2) - (static_cast<float>(row) * deltaY);
             for (int col = 0; col <= N_COL; col++) {
-                float x = (-WINDOW_WIDTH/2) + (deltaX/2) + (static_cast<float>(col) * deltaX);
-                float z = -30.0;
-                vec3 at = {x, y, z};
+                vec3 p = {
+                    (-WINDOW_WIDTH/2) + (deltaX/2) + (col * deltaX),
+                    (WINDOW_HEIGHT/2) - (deltaY/2) - (row * deltaY),
+                    FIXED_Z
+                };
 
-                VectorRay ray(rayOrigin, at);
+                VectorRay ray(FIXED_ORIGIN, p);
                 ObjectMaterial sphereMaterial {vec3(), vec3(), vec3(), 0.0};
 
-                if (Sphere sphere(sphereCenter, sphereRadius, sphereMaterial); sphere.RayIntersection(ray) < 0) {
+                if (Sphere sphere({0.0, 0.0, -100.0}, 40.0, sphereMaterial); sphere.RayIntersection(ray) < 0) {
                     pixel = SKYBLUE;
                 } else {
                     pixel = RED;
